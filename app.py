5d4628c7b7f7421c6515e700bf0ac83e85fdc002
Jack Maher <jmaher@int.openbet.com>
2025-04-15 12:29:25 +0100

    Force update of all visual and data table assets

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ def fixtures_redirect():
     fixtures["isResult"] = fixtures["isResult"].astype(str).str.lower() == "true"
     fixtures["round_number"] = pd.to_numeric(fixtures["round_number"], errors="coerce")
 
-    #next_gw = fixtures[fixtures["isResult"] == False]["round_number"].min()
-    next_gw = 33
+    next_gw = fixtures[fixtures["isResult"] == False]["round_number"].min()
+    #next_gw = 33
     return redirect(url_for("epl_fixtures", gw=next_gw))
 
 
